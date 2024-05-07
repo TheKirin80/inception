ceae97a50d7e1403d886a32213b1bf7874970019
root <root@akefeder>
2024-05-06 17:49:25 -0700

    install wordpress CLI et PHP ok need parametrage de wordpress

--- srcs/requirements/mariadb/tools/install.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'akefeder'@'%' IDENTIFIED BY 'coucou';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'akefeder'@'%' IDENTIFIED BY 'coucou';
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootcoucou';
-FLUSH PRIVILEGES;
